303958bb719f5d8addcc0a36cc2c7e3359f5988b
epelster <epelster@mathworks.com>
2025-12-09 14:08:17 +0100

    Adding custom library build to startup

--- Scripts/projectStartup.m
+++ Scripts/projectStartup.m
@@ -1,2 +1,13 @@
+% project root folder
 proj = currentProject;
 projRoot = proj.RootFolder;
+cd(projRoot);
+
+% Custom libraries
+cd('Libraries');
+if((exist('+supplemental')==7) && ~exist('supplemental_lib'))
+    disp('Building Custom Simscape Library...');
+    ssc_build Custom
+    disp('Finished Building Library.');
+end
+cd(projRoot);
